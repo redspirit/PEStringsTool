e4697edc7391a99732b28db5ad5c3c0cd1ff9207
Alexey <redspirit@live.ru>
2021-10-14 21:38:51 +0300

    ready for jar build

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import com.pestrings.pestringstool.Application;
+
+// for build jar file
+public class Main {
+    public static void main(String[] args) {
+        Application.main(args);
+    }
+}
