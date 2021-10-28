e8410c418ca16f1708bf29267b8be39e16ce35d2
Alexey <redspirit@live.ru>
2021-10-29 00:48:15 +0300

    update filter

--- src/main/java/com/pestrings/pestringstool/utils/AppSettings.java
+++ src/main/java/com/pestrings/pestringstool/utils/AppSettings.java
@@ -2,7 +2,7 @@ package com.pestrings.pestringstool.utils;
 
 public class AppSettings {
 
-    static public String version = "v0.1.2";
+    static public String version = "v0.1.3";
     static public String translateSource = "en";
     static public String translateTarget = "ru";
 
